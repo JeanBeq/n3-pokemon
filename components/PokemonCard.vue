550cafd49b79261679b5bcd4126631ea3c48d2b6
Jean B <jean.bequilleux@gmail.com>
2024-05-07 16:35:37 +0200

    chore: Update PokemonCard component and add dynamic Pokemon page

--- components/PokemonCard.vue
+++ components/PokemonCard.vue
@@ -1,24 +1,31 @@
 <template>
     <div class="bg-white rounded-lg shadow-md p-4 sm:p-6">
-        <NuxtLink :to="`/pokemon/${pokemon.id}`">
-          <img :src="pokemon.image" :alt="pokemon.name" class="w-full h-auto sm:h-64 object-cover mb-4 sm:mb-6 rounded-lg" />
-        </NuxtLink>
-        <button @click="addToTeam" :disabled="team.length >= 6" :class="{'bg-gray-500 hover:bg-gray-500': team.length >= 6}" class="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-opacity-50"><Icon name="fluent:person-add-16-regular" color="white"/></button>
+      <NuxtLink :to="`/pokemon/${pokemon.id}`">
+        <img :src="pokemon.image" :alt="pokemon.name" class="w-full h-auto sm:h-64 object-cover mb-4 sm:mb-6 rounded-lg" />
+      </NuxtLink>
+      <button v-if="!isInTeam(pokemon.id)" @click="addToTeam" :disabled="team.length >= 6" :class="{'bg-gray-500 hover:bg-gray-500': team.length >= 6}" class="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-opacity-50"><Icon name="fluent:person-add-16-regular" color="white"/></button>
+      <button v-else @click="removeFromTeam(pokemon.id)" class="px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50"><Icon name="fluent:person-delete-24-regular" color="white"/></button>
     </div>
-</template>
-
-<script setup>
-    const props = defineProps(['pokemon'])
-    const team = useState('team', () => [])
-
-    function addToTeam() {
-        if (team.value.length < 6 && !isInTeam(props.pokemon.id)) 
-        {
-            team.value.push(props.pokemon);
-        }
+  </template>
+  
+  <script setup> 
+  const props = defineProps(['pokemon'])
+  const team = useState('team', () => [])
+  
+  function addToTeam() {
+    if (team.value.length < 6 && !isInTeam(props.pokemon.id)) {
+      team.value.push(props.pokemon);
     }
-
-    function isInTeam(id) {
-        return team.value.find(pokemon => pokemon.id === id) !== undefined;
+  }
+  
+  function removeFromTeam(id) {
+    const index = team.value.findIndex(pokemon => pokemon.id === id);
+    if (index !== -1) {
+      team.value.splice(index, 1);
     }
-</script>
+  }
+  
+  function isInTeam(id) {
+    return team.value.find(pokemon => pokemon.id === id) !== undefined;
+  }
+  </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
     <h1 class="mt-10 text-2xl text-center font-bold">Liste de pokemons</h1>
     <SearchBar v-model="search" />
-    <p>{{ typesData }}</p>
     <div class="mx-auto px-4 sm:px-6 lg:px-40 lg:py-10 z-40">
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
             <PokemonCard v-for="pokemon in filteredPokemons" :key="pokemon.id" :pokemon="pokemon" />
