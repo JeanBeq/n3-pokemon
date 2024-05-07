c14e7fa1c7691199725c7ceff29249e39b250a9a
Jean B <jean.bequilleux@gmail.com>
2024-05-07 15:52:19 +0200

    chore: Refactor search functionality and update searchbar styling

---
+++ components/SearchBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex justify-center mt-5">
+        <input v-model="search" @input="$emit('update:modelValue', search)" class="w-60 block p-4 ps-10 text-sm border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 text-white" placeholder="Rechercher un pokemon" />
+    </div>
+</template>
+  
+<script>
+    export default {
+        props: ['modelValue'],
+        emits: ['update:modelValue'],
+        setup(props, { emit }) {
+            const search = computed({
+            get: () => props.modelValue,
+            set: (value) => emit('update:modelValue', value)
+            });
+
+            return { search };
+        }
+    };
+</script>
+  
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,23 @@
 <template>
     <h1 class="mt-10 text-2xl text-center font-bold">Liste de pokemons</h1>
-    <div class="flex justify-center mt-5">
-        <input v-model="search" class="w-60 block p-4 ps-10 text-sm border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 text-white" placeholder="Rechercher un pokemon" />
-    </div>
+    <SearchBar v-model="search" />
     <div class="mx-auto px-4 sm:px-6 lg:px-40 lg:py-10 z-40">
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">    
-            <PokemonCard v-for="pokemon in data" :key="pokemon.id" :pokemon="pokemon" />
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+            <PokemonCard v-for="pokemon in filteredPokemons" :key="pokemon.id" :pokemon="pokemon" />
         </div>
     </div>
 </template>
 
+
 <script setup>
+import { ref, computed } from 'vue';
+
 const search = ref('');
 const {data, refresh} = useFetch('https://pokebuildapi.fr/api/v1/pokemon/limit/100')
+
+const filteredPokemons = computed(() => {
+  return data.value.filter(pokemon => {
+    return pokemon.name.toLowerCase().includes(search.value.toLowerCase());
+  });
+});
 </script>
