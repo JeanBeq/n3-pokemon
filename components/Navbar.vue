46011a22e1cb58361c014caabf8cffe91069be11
Jean B <jean.bequilleux@gmail.com>
2024-05-07 14:01:30 +0200

    Add Navbar component and update default layout

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,18 @@
 <template>
-
+    <div class="flex justify-center">
+        <div class="fixed bottom-10 w-full max-w-screen-xl mx-auto px-4 flex items-center justify-around">
+            <div class="bg-gray-800 flex items-center justify-around p-2 rounded-full shadow-lg text-white">
+                <NuxtLink to="/" class="px-1 hover:text-white cursor-pointer flex items-center">
+                    <Icon name="material-symbols-light:menu-book-outline" color="white" size="2em"/>
+                    <span class="hidden md:block ml-2">Liste</span>
+                </NuxtLink>
+                <NuxtLink to='/team' class="px-1 hover:text-white cursor-pointer flex items-center">
+                    <Icon name="fluent:people-team-48-filled" color="white" size="2em"/>
+                    <span class="hidden md:block ml-2">Team</span>
+                </NuxtLink>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
-
+    <Navbar />
+    <Slot />
 </template>
 
 <script setup>
