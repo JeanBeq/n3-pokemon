6c51e11d26332721e0545795e7ef93a8bedc3d62
Jean B <jean.bequilleux@gmail.com>
2024-05-07 16:43:36 +0200

    chore: Add buttons to add and remove Pokemon from team

--- pages/pokemon/[id].vue
+++ pages/pokemon/[id].vue
@@ -8,6 +8,8 @@
           <span class="text-gray-700 mr-2">ID:</span>
           <span>{{ pokemon.pokedexId }}</span>
         </div>
+        <button v-if="!isInTeam(pokemon.id)" @click="addToTeam" :disabled="team.length >= 6" :class="{'bg-gray-500 hover:bg-gray-500': team.length >= 6}" class="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-opacity-50"><Icon name="fluent:person-add-16-regular" color="white"/></button>
+      <button v-else @click="removeFromTeam(pokemon.id)" class="px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-600 focus:ring-opacity-50"><Icon name="fluent:person-delete-24-regular" color="white"/></button>
         <div class="mb-4">
           <h3 class="text-lg font-semibold mb-2">Types:</h3>
           <div class="flex space-x-2">
@@ -44,4 +46,24 @@
     const route = useRoute()
     const id = route.params.id
     const { data: pokemon } = await useFetch(`https://pokebuildapi.fr/api/v1/pokemon/${id}`)
+
+    const props = defineProps(['pokemon'])
+    const team = useState('team', () => [])
+  
+    function addToTeam() {
+      if (team.value.length < 6 && !isInTeam(pokemon.id)) {
+        team.value.push(props.pokemon);
+      }
+    }
+  
+    function removeFromTeam(id) {
+      const index = team.value.findIndex(pokemon => pokemon.id === id);
+      if (index !== -1) {
+        team.value.splice(index, 1);
+      }
+    }
+  
+    function isInTeam(id) {
+      return team.value.find(pokemon => pokemon.id === id) !== undefined;
+    }
 </script>
